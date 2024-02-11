02d49a13db9a553c5149784891685291fa11300f
TechSergio <smarquez15@gmail.com>
2024-02-11 00:41:23 -0300

    se agrega funcionalidad de historial de colores

--- colores/src/Components/ColoresSelector.jsx
+++ colores/src/Components/ColoresSelector.jsx
@@ -1,13 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import { ColoresContext } from './ColoresContexto'
 
 const ColoresSelector = () => {
 
+    //Continuando con lo aprendido le agregamos funcionalidad a useRef
+    const historialColores = useRef([])
+
     //invocamos al contexto
     const { colorSeleccionado, setColorSeleccionado} = useContext(ColoresContext)
 
+    //en la segunda parte manejamos el historial de colores
     const handleColorChange = (evento) => {
-        setColorSeleccionado(evento.target.value)
+        const nuevoColor = evento.target.value
+        setColorSeleccionado(nuevoColor)
+        if (historialColores.current.length < 11){
+        historialColores.current = [nuevoColor, ...historialColores.current]
+        console.log(historialColores.current)
+        }else {
+            alert("llego al limite de 10 colores !!!")
+        }
     }
   return (
     <>
@@ -17,6 +28,14 @@ const ColoresSelector = () => {
         onChange={handleColorChange}
         style={ { width: '40px', height: '40px'}}
         />
+        <div>
+            <h4>Historial de colores</h4>
+            <ul>
+                {historialColores.current.map((color, index) => (
+                    <li key={index} style={{ backgroundColor: color, width: '30px', height: '30px', display: 'inline-block', margin: '2px' }}>{color}</li>
+                ))}
+            </ul>
+        </div>
     </>
   )
 }
